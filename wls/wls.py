ef42cae6c4ae7aa27f2bcd71ffa2f9c2542bcb23
lasterhappy <824587401@qq.com>
2024-04-13 07:58:24 +0800

    优化屋里社签到

--- wls/wls.py
+++ wls/wls.py
@@ -1,5 +1,5 @@
 '''
-屋里社
+屋里社 https://wulishe.cc/
 捉包把Authorization的值填到变量 wls_cookie里(Bearer也要填)
 目前仅支持单账号
 '''
@@ -29,11 +29,17 @@ class Wls():
         state=self.get_sign_state()
         if  state==1:
             print('已经签到过了')
-        else:
-            print('=================== 开始签到 ======================')
-            response=self.session.post(self.sign_url,headers=self.headers)
-            text=response.text.replace('"', '').replace('\n', '')
-            print(f'签到成功!获得{text}积分')
+            return
+
+        print('未签到，开始执行签到！')
+        response=self.session.post(self.sign_url,headers=self.headers)
+        response.raise_for_status()  # 检查响应状态码
+        data = response.json()  # 使用json方法直接获取字典
+        print('=================== 开始签到 ======================')
+        print(f'签到成功!获得{data["credit"]}积分')
+        print(f'签到日期:{data["date"]}')
+        print(f'连续签到:{data["mission"]["always"]}天')
+        print(f'总积分:{data["mission"]["my_credit"]}')
 
 
     def main(self):
